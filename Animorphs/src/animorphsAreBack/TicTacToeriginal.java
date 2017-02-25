697757b9978c789b1287c2af872092a9e828fb7b
Jake <isuandme@gmail.com>
2017-02-25 17:08:36 -0600

    changes to ttto finalized winner()

--- Animorphs/src/animorphsAreBack/TicTacToeriginal.java
+++ Animorphs/src/animorphsAreBack/TicTacToeriginal.java
@@ -57,30 +57,61 @@ public class TicTacToeriginal extends TicTacToe {
 		for (int i = 0; i < size; i++) {
 			if (getBoard()[i][0] == 1 && getBoard()[i][1] == 1 && getBoard()[i][2] == 1) {
 				xWins = true;
-			} 
+			}
 			if (getBoard()[i][0] == 2 && getBoard()[i][1] == 2 && getBoard()[i][2] == 2) {
 				oWins = true;
 			}
 			if (getBoard()[0][i] == 1 && getBoard()[1][i] == 1 && getBoard()[2][i] == 1) {
 				xWins = true;
-			} 
+			}
 			if (getBoard()[0][i] == 2 && getBoard()[1][i] == 2 && getBoard()[2][i] == 2) {
 				oWins = true;
-			}	
-		}
-		// Diagonal lines
-		for (int i = 0; i < size; i++) {
-			for(int j = 0; j < size; j++){
-				if(size % 2 == 0){
-					
-				} else {
-					
-				}
-				
 			}
 		}
 
-		return -1; // return zeor if there isn't a match
+		int j = size;
+		
+		int xCountForwardSlash = 0;
+		int oCountForwardSlash = 0;
+		int xCountBackSlash = 0;
+		int oCountBackSlash = 0;
+		
+		// Diagonal lines
+		for (int i = 0; i < size; i++) {
+			
+			// Checks the forward diagonal of the matrix for x
+			if (getBoard()[i][j - 1] == 1) {
+				oCountForwardSlash += 1;
+				if(oCountForwardSlash == size)
+					return 1;
+			}
+			
+			// Checks the forward diagonal of the matrix for o
+			if (getBoard()[i][j - 1] == 2) {
+				oCountForwardSlash += 1;
+				if(oCountForwardSlash == size);
+					return 2;
+			}
+			
+			j--;
+			
+			// Checks the backward diagonal of the matrix of x
+			if(getBoard()[i][i] == 1){
+				xCountBackSlash += 1;
+				if(xCountBackSlash == size)
+					return 1;
+			}
+			
+			// checks the backward diagonal of the matrix for o.
+			if(getBoard()[i][i] == 2){
+				oCountBackSlash += 1;
+				if(oCountBackSlash == size)
+					return 2;
+			}
+			
+		}
+
+		return -1; // return '-1'  if there isn't a match
 
 	}
 
