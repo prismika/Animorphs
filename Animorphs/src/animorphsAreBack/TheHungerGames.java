53825e392c3d28facd7daf2ddba13533c53795a4
Jake <isuandme@gmail.com>
2017-02-26 02:22:25 -0600

    added the tread example class

--- Animorphs/src/animorphsAreBack/TheHungerGames.java
+++ Animorphs/src/animorphsAreBack/TheHungerGames.java
@@ -1,11 +1,32 @@
 package animorphsAreBack;
 
-public class TheHungerGames {
-
+public class TheHungerGames implements Runnable{
+	
 	public static void main(String args[]){
+		
+		// Create two players
+		
+		CandidateAlgorithm playerX = new CandidateAlgorithm(1);
+		CandidateAlgorithm playerO = new CandidateAlgorithm(2);
+		
+		Thread thd1 = new Thread();
 		
 		
 		
 	}
+	
+		
+	
+	@Override
+	public void run() {
+		
+		
+		
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
 	
 }
